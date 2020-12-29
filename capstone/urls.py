ea3ed1e1f10f263ba632914fae865538339f350b
Purvesh <purvesh.gandhi2012@gmail.com>
2020-12-29 22:08:29 +0530

    created login page

--- capstone/urls.py
+++ capstone/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import common
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('common.urls')),
+    path('admin/', admin.site.urls)
 ]
