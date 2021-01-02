9812aa143edfaaf2ef61a90977ba494b146d8807
Purvesh <purvesh.gandhi2012@gmail.com>
2021-01-02 23:32:55 +0530

    validation in student form

--- common/views.py
+++ common/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from students.forms import StudentForm
 from staff.forms import StaffForm
 from students.models import Student
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password, UserAttributeSimilarityValidator
 
 # Create your views here.
 
@@ -15,13 +17,64 @@ def registration_view(request,*args,**kwargs):
     return render(request,"common/register.html")
 
 def student_registration(request,*args,**kwargs):
-    form = StudentForm()
     if request.method == "POST":
-        form = StudentForm(request.POST)
+        form = StudentForm(request.POST or None)
         if form.is_valid():
-            print(form.cleaned_data)
-            Student.objects.create(**form.cleaned_data)
+            details = form.cleaned_data
+            new_passwd = details['passwd']
+            new_username = details['username']
+            new_fName = details['firstName']
+            new_mName = details['middleName']
+            new_lName = details['lastName']
+            new_email = details['email']
+            new_enrolment = details['enrolment']
+            new_dob = details['date']
+            new_gender = details['gender']
+            new_mobile = details['mobile']
+            new_branch = details['branch']
+            new_sem = details['sem']
+
+            try:
+                validate_password(new_passwd, form)
+            except ValidationError as e:
+                form.add_error('passwd', e)
+                return render(request, "common/studentregistration.html", {'form': form})
+            
+            try:
+                user_credentials = [new_username, new_fName, new_lName, new_mName, new_mobile, new_enrolment]
+
+                for item in user_credentials:
+                    if item.lower() in new_passwd.lower():
+                        raise ValidationError('Password too similar to credentials')
+            except ValidationError as e:
+                form.add_error('passwd', e)
+                return render(request, "common/studentregistration.html", {'form': form})
+            
+            
+            
+
+            newStudent = Student(firstName=str(new_fName.capitalize()),
+                        middleName=str(new_mName.capitalize()),
+                        lastName=str(new_lName.capitalize()),
+                        username=str(new_username),
+                        passwd=str(new_passwd),
+                        enrolment=str(new_enrolment),
+                        date=str(new_dob),
+                        mobile=str(new_mobile),
+                        branch=str(new_branch),
+                        sem=str(new_sem),
+                        email=str(new_email.lower()),
+                        gender=str(new_gender)
+                        )
+            newStudent.save()
+
+
+            # Student.objects.create(**form.cleaned_data)
             return redirect("../login")
+    else:
+        form = StudentForm(request.POST or None)
+        for field in form.errors:
+                form[field].field.widget.attrs['class'] += 'error'
 
     return render(request,"common/studentregistration.html",{'form':form})
 
